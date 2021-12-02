db0a0f9d8835e9cb3e9f7aa540c2781b350f8f06
daload <dlopezad7@alumnes.ub.edu>
2021-12-02 20:26:29 +0100

    - Add country - Add city - Add person - Add person contact (Relation) - Add city - person relation - Add country - city relation - Delete person - Clear DB

--- main.py
+++ main.py
@@ -1,6 +1,49 @@
 
 from test import App
 
+def clear_db(app):
+    app.delete_all()
+    # TODO: Clear SQL
+
+def add_country(app, cid, name, population):
+    #TODO: ADD TO SQL
+    app.create_country(cid, name)
+
+def add_city(app, cid, name, population):
+    # TODO: ADD TO SQL
+    app.create_city(cid, name)
+
+def add_person(app, pid, name, age):
+    # TODO: ADD TO SQL
+    app.create_person(pid, name)
+
+def add_person_contact(app, pid1, pid2):
+    app.create_person_contact(pid1, pid2)
+
+def delete_person(app, pid):
+    app.delete_person(pid)
+    # TODO: DELETE FROM SQL
+
+def add_city_person_rel(app, cid, pid):
+    app.create_city_person_relation(cid, pid)
+
+def add_country_city_rel(app, coid, ciid):
+    app.create_country_city_relation(coid, ciid)
+
+
+def populator(app):
+    clear_db(app)
+    add_country(app, 1, "Spain without the S", 1)
+    add_city(app, 1, "Barcelona", 1)
+    add_person(app, 1, "David", 12)
+    add_person(app, 2, "Alberto", 34)
+    add_person_contact(app, 1, 2)
+    add_person(app, 3, "Samuel", 56)
+    add_person(app, 4, "Eric", 78)
+    add_person_contact(app, 3, 4)
+    add_city_person_rel(app, 1, 1)
+    add_country_city_rel(app, 1, 1)
+    delete_person(app, 3)
 
 if __name__ == "__main__":
     # Aura queries use an encrypted connection using the "neo4j+s" URI scheme
@@ -8,7 +51,5 @@ if __name__ == "__main__":
     user = "neo4j"
     password = "bwlRM9QFS7BleaZDEX_fyxXaKprfuO2Oyl9U0lQ-rVc"
     app = App(uri, user, password)
-    app.create_person("David")
-    app.create_person("Alberto")
-    app.find_person("David")
+    populator(app)
     app.close()
--- test.py
+++ test.py
@@ -11,6 +11,187 @@ class App:
         # Don't forget to close the driver connection when you are finished with it
         self.driver.close()
 
+    def create_country(self, country_id, country_name):
+        with self.driver.session() as session:
+            result = session.write_transaction(
+                self._create_country, country_id, country_name
+            )
+            print("Created COUNTRY with ID: {0} and NAME: {1}".format(country_id, country_name))
+
+    @staticmethod
+    def _create_country(tx, country_id, country_name):
+        query = (
+            "CREATE (c:Country { country_id: $country_id, name: $country_name }) "
+            "RETURN c"
+        )
+        result = tx.run(query, country_id=country_id, country_name=country_name)
+        try:
+            return [{"c": row["c"]["name"]} for row in result]
+        except ServiceUnavailable as exception:
+            logging.error("{query} raised an error: \n {exception}".format(
+                query=query, exception=exception))
+            raise
+
+    def create_country_city_relation(self, country_id, city_id):
+        with self.driver.session() as session:
+            result = session.write_transaction(
+                self._create_country_city_relation, country_id, city_id
+            )
+            print("Created relation: {0} CONTAINS {1}".format(result[0]['co'], result[0]['ci']))
+
+    @staticmethod
+    def _create_country_city_relation(tx, country_id, city_id):
+        query = (
+            "MATCH (co:Country {country_id: $country_id}) "
+            "MATCH (ci:City {city_id: $city_id}) "
+            "CREATE (co)-[:CONTAINS]->(ci) "
+            "RETURN co, ci "
+        )
+
+        result = tx.run(query, country_id=country_id, city_id=city_id)
+        try:
+            return [{"co": row["co"]["name"], "ci": row["ci"]["name"]} for row in result]
+        except ServiceUnavailable as exception:
+            logging.error("{query} raised an error: \n {exception}".format(
+                query=query, exception=exception))
+            raise
+
+    def create_city(self, city_id, city_name):
+        with self.driver.session() as session:
+            result = session.write_transaction(
+                self._create_city, city_id, city_name
+            )
+            print("Created CITY with ID: {0} and NAME: {1}".format(city_id, city_name))
+
+    @staticmethod
+    def _create_city(tx, city_id, city_name):
+        query = (
+            "CREATE (c:City { city_id: $city_id, name: $city_name }) "
+            "RETURN c"
+        )
+        result = tx.run(query, city_id=city_id, city_name=city_name)
+        try:
+            return [{"c": row["c"]["name"]} for row in result]
+        except ServiceUnavailable as exception:
+            logging.error("{query} raised an error: \n {exception}".format(
+                query=query, exception=exception))
+            raise
+
+    def create_city_person_relation(self, city_id, person_id):
+        with self.driver.session() as session:
+            result = session.write_transaction(
+                self._create_city_person_relation, city_id, person_id
+            )
+            print("Created relation: {0} IS RESIDED BY {1}".format(result[0]['c'], result[0]['p']))
+
+    @staticmethod
+    def _create_city_person_relation(tx, city_id, person_id):
+        query = (
+            "MATCH (c:City {city_id: $city_id}) "
+            "MATCH (p:Person {person_id: $person_id}) "
+            "CREATE (c)-[:RESIDED_BY]->(p) "
+            "RETURN c, p "
+        )
+
+        result = tx.run(query, city_id=city_id, person_id=person_id)
+        try:
+            return [{"c": row["c"]["name"], "p": row["p"]["name"]} for row in result]
+        except ServiceUnavailable as exception:
+            logging.error("{query} raised an error: \n {exception}".format(
+                query=query, exception=exception))
+            raise
+
+    def create_person(self, person_id, person_name):
+        with self.driver.session() as session:
+            result = session.write_transaction(
+                self._create_person, person_id, person_name
+            )
+            print("Created PERSON with ID: {0} and NAME: {1}".format(person_id, person_name))
+
+    @staticmethod
+    def _create_person(tx, person_id, person_name):
+        query = (
+            "CREATE (p:Person { person_id: $person_id, name: $person_name }) "
+            "RETURN p"
+        )
+        result = tx.run(query, person_id=person_id, person_name=person_name)
+        try:
+            return [{"p": row["p"]["name"]} for row in result]
+        except ServiceUnavailable as exception:
+            logging.error("{query} raised an error: \n {exception}".format(
+                query=query, exception=exception))
+            raise
+
+    def delete_person(self, person_id):
+        with self.driver.session() as session:
+            result = session.write_transaction(
+                self._delete_person, person_id
+            )
+            print("Deleted PERSON with ID: {0}".format(person_id))
+
+    @staticmethod
+    def _delete_person(tx, person_id):
+        query = (
+            "MATCH (p:Person {person_id: $person_id}) "
+            "DETACH DELETE p "
+        )
+        result = tx.run(query, person_id=person_id)
+        try:
+            return [{"p1": row["p1"]["name"], "p2": row["p2"]["name"]} for row in result]
+        except ServiceUnavailable as exception:
+            logging.error("{query} raised an error: \n {exception}".format(
+                query=query, exception=exception))
+            raise
+
+    def create_person_contact(self, person1_id, person2_id):
+        with self.driver.session() as session:
+            result = session.write_transaction(
+                self._create_person_contact, person1_id, person2_id
+            )
+            print("Created relation: {0} HAS BEEN WITH {1}".format(result[0]['p1'], result[0]['p2']))
+
+    @staticmethod
+    def _create_person_contact(tx, person1_id, person2_id):
+        query = (
+            "MATCH (p1:Person {person_id: $person1_id}) "
+            "MATCH (p2:Person {person_id: $person2_id}) "
+            "CREATE (p1)-[:HAS_BEEN_WITH]->(p2) "
+            "CREATE (p2)-[:HAS_BEEN_WITH]->(p1) "
+            "RETURN p1, p2 "
+        )
+
+        result = tx.run(query, person1_id=person1_id, person2_id=person2_id)
+        try:
+            return [{"p1": row["p1"]["name"], "p2": row["p2"]["name"]} for row in result]
+        except ServiceUnavailable as exception:
+            logging.error("{query} raised an error: \n {exception}".format(
+                query=query, exception=exception))
+            raise
+
+    def delete_all(self):
+        with self.driver.session() as session:
+            result = session.write_transaction(
+                self._delete_all
+            )
+            print("Graph DB cleared")
+
+    @staticmethod
+    def _delete_all(tx):
+        query = (
+            "MATCH (p) "
+            "DETACH DELETE p "
+        )
+        result = tx.run(query)
+        try:
+            return ["Graph DB cleared"]
+        except ServiceUnavailable as exception:
+            logging.error("{query} raised an error: \n {exception}".format(
+                query=query, exception=exception))
+            raise
+
+
+
+
     def create_friendship(self, person1_name, person2_name):
         with self.driver.session() as session:
             # Write transactions allow the driver to handle retries and transient errors
@@ -18,13 +199,6 @@ class App:
                 self._create_and_return_friendship, person1_name, person2_name)
             for row in result:
                 print("Created friendship between: {p1}, {p2}".format(p1=row['p1'], p2=row['p2']))
-
-    def create_person(self, person_name):
-        with self.driver.session() as session:
-            result = session.write_transaction(
-                self._create_person, person_name
-            )
-            print("Created person with name: {0}".format(person_name))
 
     @staticmethod
     def _create_and_return_friendship(tx, person1_name, person2_name):
@@ -52,19 +226,7 @@ class App:
             for row in result:
                 print("Found person: {row}".format(row=row))
 
-    @staticmethod
-    def _create_person(tx, person_name):
-        query = (
-            "CREATE (p:Person { name: $person_name }) "
-            "RETURN p"
-        )
-        result = tx.run(query, person_name=person_name)
-        try:
-            return [{"p": row["p"]["name"]} for row in result]
-        except ServiceUnavailable as exception:
-            logging.error("{query} raised an error: \n {exception}".format(
-                query=query, exception=exception))
-            raise
+
 
 
     @staticmethod
